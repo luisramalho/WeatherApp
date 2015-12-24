03d4b667230f8dce36ec2ce294cb07300ae51d87
Luís Ramalho <info@luisramalho.com>
2015-12-24 17:47:07 +0200

    Added data classes

---
+++ app/src/main/java/com/luisramalho/weatherapp/data/ResponseClasses.kt
@@ -0,0 +1,40 @@
+package com.luisramalho.weatherapp.data
+
+/**
+ * Created by Luís Ramalho on 24/12/15.
+ * <info@luisramalho.com>
+ */
+class ResponseClasses {
+
+    data class ForecastResult(val city: City, val list: List<Forecast>)
+
+    data class City(val id: Long,
+                    val name: String,
+                    val coord: Coordinates,
+                    val country: String,
+                    val population: Int)
+
+    data class Coordinates(val lon: Float, val lat: Float)
+
+    data class Forecast(val dt: Long,
+                        val temp: Temperature,
+                        val pressure: Float,
+                        val humidity: Int,
+                        val weather: List<Weather>,
+                        val speed: Float,
+                        val deg: Int,
+                        val clouds: Int,
+                        val rain: Float)
+
+    data class Temperature(val day: Float,
+                           val min: Float,
+                           val max: Float,
+                           val night: Float,
+                           val eve: Float,
+                           val morn: Float)
+
+    data class Weather(val id: Long,
+                       val main: String,
+                       val description: String,
+                       val icon: String)
+}
